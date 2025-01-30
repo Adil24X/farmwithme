954ad74a0625dd3cb31c9c46f839d2007669c53f
Adil Shaikh <161579337+Adil24X@users.noreply.github.com>
2025-01-30 19:44:31 +0530

    Update server.js updated cors origin to netlify

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    // origin: "http://localhost:5173",
+    origin: 'https://delicate-mooncake-1b2487.netlify.app', // Allow only your frontend
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
