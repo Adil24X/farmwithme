653929d8d1394f195756e0056dded7e77d4a75fa
Adil Shaikh <161579337+Adil24X@users.noreply.github.com>
2025-01-30 19:22:50 +0530

    Update index.js

--- frontend/src/store/shop/address-slice/index.js
+++ frontend/src/store/shop/address-slice/index.js
@@ -10,7 +10,7 @@ export const addNewAddress = createAsyncThunk(
   "/addresses/addNewAddress",
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/shop/address/add",
+      "https://demo-1-hh2v.onrender.com/api/shop/address/add",
       formData
     );
 
@@ -22,7 +22,7 @@ export const fetchAllAddresses = createAsyncThunk(
   "/addresses/fetchAllAddresses",
   async (userId) => {
     const response = await axios.get(
-      `http://localhost:5000/api/shop/address/get/${userId}`
+      `https://demo-1-hh2v.onrender.com/api/shop/address/get/${userId}`
     );
 
     return response.data;
@@ -45,7 +45,7 @@ export const deleteAddress = createAsyncThunk(
   "/addresses/deleteAddress",
   async ({ userId, addressId }) => {
     const response = await axios.delete(
-      `http://localhost:5000/api/shop/address/delete/${userId}/${addressId}`
+      `https://demo-1-hh2v.onrender.com/api/shop/address/delete/${userId}/${addressId}`
     );
 
     return response.data;
