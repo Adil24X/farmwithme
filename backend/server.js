1920dca615b478702738bb8cccb67883fb74fb8a
Adil Shaikh <161579337+Adil24X@users.noreply.github.com>
2025-01-30 20:10:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: 'https://taupe-seahorse-06ac1c.netlify.app', // Allow only your frontend
+    origin: 'https://farm2doorforu.netlify.app', // Allow only your frontend
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
