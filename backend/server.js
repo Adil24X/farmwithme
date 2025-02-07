4a6732c91627aafd17554cb41974776eafd3df24
Adil Shaikh <161579337+Adil24X@users.noreply.github.com>
2025-02-08 00:12:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 4000;
 app.use(
 
   cors({
-    origin: 'http://localhost:5173', // Allow only your frontend
+    origin: 'https://nimble-kelpie-31a40e.netlify.app', // Allow only your frontend
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
