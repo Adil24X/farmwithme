903d230a1de1b6825a4a867287b49852a1834659
Adil Shaikh <161579337+Adil24X@users.noreply.github.com>
2025-02-13 11:11:24 +0530

    Update paypal.js

--- backend/helpers/paypal.js
+++ backend/helpers/paypal.js
@@ -11,12 +11,23 @@
 
 // module.exports = paypal;
 
-const hello = require('@paypal/paypal-server-sdk');
+// const hello = require('@paypal/paypal-server-sdk');
 
-const clientId ="AdyG-BtFO0Ad-pMHSX0UBCZdq_SsMlt9gSU5SpqTpsiUuqsfNfCIsqT6KEbue5HEnLkDJfxLcBh05urz";
-const clientSecret ="EC0zKihaG2DuwQkNGPY4Pz1_POi-FL_ZnLNCHYZoUiKwihbGRcYQU3Vr2o5cbl-MhHJYy2XsgfCf7WHQ";
+// const clientId ="AdyG-BtFO0Ad-pMHSX0UBCZdq_SsMlt9gSU5SpqTpsiUuqsfNfCIsqT6KEbue5HEnLkDJfxLcBh05urz";
+// const clientSecret ="EC0zKihaG2DuwQkNGPY4Pz1_POi-FL_ZnLNCHYZoUiKwihbGRcYQU3Vr2o5cbl-MhHJYy2XsgfCf7WHQ";
 
-const environment = new hello.core.SandboxEnvironment(clientId, clientSecret);
-const paypal = new hello.core.PayPalHttpClient(environment);
+// const environment = new hello.core.SandboxEnvironment(clientId, clientSecret);
+// const paypal = new hello.core.PayPalHttpClient(environment);
+
+// module.exports = paypal;
+
+const demo = require('@paypal/checkout-server-sdk');
+
+const clientId = "AdyG-BtFO0Ad-pMHSX0UBCZdq_SsMlt9gSU5SpqTpsiUuqsfNfCIsqT6KEbue5HEnLkDJfxLcBh05urz";
+const clientSecret = "EC0zKihaG2DuwQkNGPY4Pz1_POi-FL_ZnLNCHYZoUiKwihbGRcYQU3Vr2o5cbl-MhHJYy2XsgfCf7WHQ";
+
+// Set up the PayPal environment
+const environment = new demo.core.SandboxEnvironment(clientId, clientSecret); // Use LiveEnvironment for production
+const paypal = new demo.core.PayPalHttpClient(environment);
 
 module.exports = paypal;
