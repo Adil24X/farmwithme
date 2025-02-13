36961f826351058d57ccdb4c6a43588ad962358c
Adil Shaikh <161579337+Adil24X@users.noreply.github.com>
2025-02-13 13:43:40 +0530

    Update paypal.js

--- backend/helpers/paypal.js
+++ backend/helpers/paypal.js
@@ -1,27 +1,27 @@
 
-const paypal = require("paypal-rest-sdk");
+// const paypal = require("paypal-rest-sdk");
 
 
 
-paypal.configure({
-  mode: "sandbox",
-  client_id: "AdyG-BtFO0Ad-pMHSX0UBCZdq_SsMlt9gSU5SpqTpsiUuqsfNfCIsqT6KEbue5HEnLkDJfxLcBh05urz",
-  client_secret: "EC0zKihaG2DuwQkNGPY4Pz1_POi-FL_ZnLNCHYZoUiKwihbGRcYQU3Vr2o5cbl-MhHJYy2XsgfCf7WHQ",
-});
+// paypal.configure({
+//   mode: "sandbox",
+//   client_id: "AdyG-BtFO0Ad-pMHSX0UBCZdq_SsMlt9gSU5SpqTpsiUuqsfNfCIsqT6KEbue5HEnLkDJfxLcBh05urz",
+//   client_secret: "EC0zKihaG2DuwQkNGPY4Pz1_POi-FL_ZnLNCHYZoUiKwihbGRcYQU3Vr2o5cbl-MhHJYy2XsgfCf7WHQ",
+// });
 
-module.exports = paypal;
-
-
-
-// const paypal = require('@paypal/checkout-server-sdk');
-
-// const clientId = "AdyG-BtFO0Ad-pMHSX0UBCZdq_SsMlt9gSU5SpqTpsiUuqsfNfCIsqT6KEbue5HEnLkDJfxLcBh05urz";
-// const clientSecret = "EC0zKihaG2DuwQkNGPY4Pz1_POi-FL_ZnLNCHYZoUiKwihbGRcYQU3Vr2o5cbl-MhHJYy2XsgfCf7WHQ";
-
-// // Set up the PayPal environment (Sandbox or Live)
-// const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret); // Use LiveEnvironment for production
-// const client = new paypal.core.PayPalHttpClient(environment);
+// module.exports = paypal;
 
 
-// module.exports = { paypal, client };
+
+const demo = require('@paypal/checkout-server-sdk');
+
+const clientId = "AdyG-BtFO0Ad-pMHSX0UBCZdq_SsMlt9gSU5SpqTpsiUuqsfNfCIsqT6KEbue5HEnLkDJfxLcBh05urz";
+const clientSecret = "EC0zKihaG2DuwQkNGPY4Pz1_POi-FL_ZnLNCHYZoUiKwihbGRcYQU3Vr2o5cbl-MhHJYy2XsgfCf7WHQ";
+
+// Set up the PayPal environment (Sandbox or Live)
+const environment = new demo.core.SandboxEnvironment(clientId, clientSecret); // Use LiveEnvironment for production
+const paypal = new demo.core.PayPalHttpClient(environment);
+
+
+module.exports =  {paypal,demo} ;
 
