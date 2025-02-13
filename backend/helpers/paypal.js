0aadc6f59af3272a12a741f6ee801c64b1006ce0
Adil Shaikh <161579337+Adil24X@users.noreply.github.com>
2025-02-13 12:18:00 +0530

    Update paypal.js

--- backend/helpers/paypal.js
+++ backend/helpers/paypal.js
@@ -21,15 +21,15 @@
 
 // module.exports = paypal;
 
-const demo = require('@paypal/checkout-server-sdk');
+const paypal = require('@paypal/checkout-server-sdk');
 
 const clientId = "AdyG-BtFO0Ad-pMHSX0UBCZdq_SsMlt9gSU5SpqTpsiUuqsfNfCIsqT6KEbue5HEnLkDJfxLcBh05urz";
 const clientSecret = "EC0zKihaG2DuwQkNGPY4Pz1_POi-FL_ZnLNCHYZoUiKwihbGRcYQU3Vr2o5cbl-MhHJYy2XsgfCf7WHQ";
 
 // Set up the PayPal environment (Sandbox or Live)
-const environment = new demo.core.SandboxEnvironment(clientId, clientSecret); // Use LiveEnvironment for production
-const paypal = new demo.core.PayPalHttpClient(environment);
+const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret); // Use LiveEnvironment for production
+const client = new paypal.core.PayPalHttpClient(environment);
 
 
-module.exports = paypal;
+module.exports = { paypal, client };
 
