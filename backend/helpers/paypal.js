9236003892000912ce3527f12d918c02a4100b64
Adil Shaikh <161579337+Adil24X@users.noreply.github.com>
2025-02-13 12:27:26 +0530

    Update paypal.js

--- backend/helpers/paypal.js
+++ backend/helpers/paypal.js
@@ -1,35 +1,27 @@
-// const paypal = require("paypal-server-sdk");
-// const paypal = require("paypal-rest-sdk");
-// const paypal = require('@paypal/paypal-server-sdk').default;
+
+const paypal = require("paypal-rest-sdk");
 
 
-// paypal.configure({
-//   mode: "sandbox",
-//   client_id: "AdyG-BtFO0Ad-pMHSX0UBCZdq_SsMlt9gSU5SpqTpsiUuqsfNfCIsqT6KEbue5HEnLkDJfxLcBh05urz",
-//   client_secret: "EC0zKihaG2DuwQkNGPY4Pz1_POi-FL_ZnLNCHYZoUiKwihbGRcYQU3Vr2o5cbl-MhHJYy2XsgfCf7WHQ",
-// });
 
-// module.exports = paypal;
+paypal.configure({
+  mode: "sandbox",
+  client_id: "AdyG-BtFO0Ad-pMHSX0UBCZdq_SsMlt9gSU5SpqTpsiUuqsfNfCIsqT6KEbue5HEnLkDJfxLcBh05urz",
+  client_secret: "EC0zKihaG2DuwQkNGPY4Pz1_POi-FL_ZnLNCHYZoUiKwihbGRcYQU3Vr2o5cbl-MhHJYy2XsgfCf7WHQ",
+});
 
-// const hello = require('@paypal/paypal-server-sdk');
-
-// const clientId ="AdyG-BtFO0Ad-pMHSX0UBCZdq_SsMlt9gSU5SpqTpsiUuqsfNfCIsqT6KEbue5HEnLkDJfxLcBh05urz";
-// const clientSecret ="EC0zKihaG2DuwQkNGPY4Pz1_POi-FL_ZnLNCHYZoUiKwihbGRcYQU3Vr2o5cbl-MhHJYy2XsgfCf7WHQ";
-
-// const environment = new hello.core.SandboxEnvironment(clientId, clientSecret);
-// const paypal = new hello.core.PayPalHttpClient(environment);
-
-// module.exports = paypal;
-
-const paypal = require('@paypal/checkout-server-sdk');
-
-const clientId = "AdyG-BtFO0Ad-pMHSX0UBCZdq_SsMlt9gSU5SpqTpsiUuqsfNfCIsqT6KEbue5HEnLkDJfxLcBh05urz";
-const clientSecret = "EC0zKihaG2DuwQkNGPY4Pz1_POi-FL_ZnLNCHYZoUiKwihbGRcYQU3Vr2o5cbl-MhHJYy2XsgfCf7WHQ";
-
-// Set up the PayPal environment (Sandbox or Live)
-const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret); // Use LiveEnvironment for production
-const client = new paypal.core.PayPalHttpClient(environment);
+module.exports = paypal;
 
 
-module.exports = { paypal, client };
+
+// const paypal = require('@paypal/checkout-server-sdk');
+
+// const clientId = "AdyG-BtFO0Ad-pMHSX0UBCZdq_SsMlt9gSU5SpqTpsiUuqsfNfCIsqT6KEbue5HEnLkDJfxLcBh05urz";
+// const clientSecret = "EC0zKihaG2DuwQkNGPY4Pz1_POi-FL_ZnLNCHYZoUiKwihbGRcYQU3Vr2o5cbl-MhHJYy2XsgfCf7WHQ";
+
+// // Set up the PayPal environment (Sandbox or Live)
+// const environment = new paypal.core.SandboxEnvironment(clientId, clientSecret); // Use LiveEnvironment for production
+// const client = new paypal.core.PayPalHttpClient(environment);
+
+
+// module.exports = { paypal, client };
 
